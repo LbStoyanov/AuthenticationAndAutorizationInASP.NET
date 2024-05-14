e3b017c627d69c054583179c4322bf901f192f85
LbStoyanov <lbstoyanov1@gmail.com>
2024-05-14 23:13:56 +0200

    Add Authentication in the project

--- WebApp_Security/WebApp_Security/Program.cs
+++ WebApp_Security/WebApp_Security/Program.cs
@@ -3,6 +3,12 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddRazorPages();
 
+builder.Services.AddAuthentication().AddCookie("MyPersonalCookie", options =>
+{
+    options.Cookie.Name = "MyPersonalCookie"; //The cookie must be stored in a constant !!!
+});
+
+
 var app = builder.Build();
 
 // Configure the HTTP request pipeline.
